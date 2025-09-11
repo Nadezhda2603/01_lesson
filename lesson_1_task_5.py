b34d04417b3e7db6175be680b2fc729908c1f140
Nadezhda2603 <n1542n22@gmail.com>
2025-09-11 22:58:02 +0300

    first commit

---
+++ lesson_1_task_5.py
@@ -0,0 +1,14 @@
+def num_print(num):
+    print(num, end = '')
+
+num_print(8)
+num_print(8)
+num_print(0)
+num_print(0)
+num_print(5)
+num_print(5)
+num_print(5)
+num_print(3)
+num_print(5)
+num_print(3)
+num_print(5)
