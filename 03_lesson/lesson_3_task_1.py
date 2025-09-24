baef23bdde1e40866c905aeb3ccebf159de5c4be
Nadezhda2603 <n1542n22@gmail.com>
2025-09-24 23:30:40 +0300

    lesson3 branch files

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,6 @@
+from user import User
+
+my_user = User("Петр", "Иванов")
+my_user.print_name()
+my_user.print_lname()
+my_user.print_full()
---
+++ 03_lesson/smartphone.py
@@ -0,0 +1,5 @@
+class Smartphone:
+    def __init__(self, brend, model, number):
+        self.bernd = brend
+        self.model = model
+        self.number = number
